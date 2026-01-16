c4598ed1bb3b5a670848104855d3748177ded0e3
nrazdhan <61641131+nrazdhan@users.noreply.github.com>
2026-01-16 23:10:09 +0530

    Fix semicolon bug

--- src/main/java/com/example/mediaJournal/controller/helloController.java
+++ src/main/java/com/example/mediaJournal/controller/helloController.java
@@ -9,7 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
